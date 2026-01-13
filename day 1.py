e73bbcf139815c3d77ca4899a14fea9269c47907
lahari varigonda <laharivarigonda@gmail.com>
2026-01-13 11:39:27 +0530

    arithmatic operator

--- day 1.py
+++ day 1.py
@@ -1,7 +1,27 @@
-print ("Artificial Intelligence is the future")
-print ("and Python is the future of AI")
+# print ("Artificial Intelligence is the future")
+# print ("and Python is the future of AI")
 
 
 
-for i in range(5):
-    print("This is line number", i+1)
+# for i in range(5):
+#     print("This is line number", i+1)
+
+
+name = "lahari"
+age = "22"
+learning_python = "True"
+
+print ("Name:", name)
+print ("Age:", age)
+print ("Learning_Python:", learning_python)
+
+
+a = 20
+b = 10
+
+print("Addition:", a + b)
+print("subtraction:", a - b)
+print("Division:", a / b)
+print("Multiplication:", a * b)
+print("Floor Division:", a // b)
+print("Power:", a ** b)
